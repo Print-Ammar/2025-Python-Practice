0853d5e2494b48c929b195366adc23abccd07834
Print-Ammar <ammaraltafsaleh2007@gmail.com>
2025-10-07 17:53:24 -0400

    added loop logic to rw visual

--- PythonCrashCourse/chapter_15/rw_visuals.py
+++ PythonCrashCourse/chapter_15/rw_visuals.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values,rw.y_values, s = 15)
-plt.show()
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values,rw.y_values, s = 15)
+    plt.show()
+
+
+    keep_running = input("Should you keep running? y/n")
+    if keep_running.lower() == 'n':
+        break
