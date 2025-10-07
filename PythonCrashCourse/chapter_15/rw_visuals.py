2230754a4efbfb14270e3f185780554f89ca6316
Print-Ammar <ammaraltafsaleh2007@gmail.com>
2025-10-07 18:53:29 -0400

    changed from scatter plot to line

--- PythonCrashCourse/chapter_15/rw_visuals.py
+++ PythonCrashCourse/chapter_15/rw_visuals.py
@@ -2,12 +2,19 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(5000)
     rw.fill_walk()
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize = (15,9))
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values,rw.y_values, s = 15, c=point_numbers, cmap=plt.cm.Reds, edgecolors='none')
+    ax.plot(rw.x_values,rw.y_values, linewidth = 3)
+
+    ax.plot(0, 0, color='green',)
+    ax.plot(rw.x_values[-1], rw.y_values[-1], color='blue')
+
+    
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
 
 
