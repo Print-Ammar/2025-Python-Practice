cf6811aa29c7c819caadef568d8d581a11389044
Print-Ammar <ammaraltafsaleh2007@gmail.com>
2025-10-08 21:08:28 -0400

    stika_highs_lows done

---
+++ PythonCrashCourse/chapter_16/stika_highs_lows.py
@@ -0,0 +1,32 @@
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+first_date = datetime.strptime('2021-07-01', '%Y-%m-%d')
+filename = r"2025-Python-Practice/PythonCrashCourse/chapter_16/sitka_weather_2021_simple.csv"
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates = []
+    highs = []
+    lows = []
+
+    for index, column_header in enumerate(header_row):
+        
+        for row in reader:
+            current_date = datetime.strptime(row[2], '%Y-%m-%d')
+            high = int(row[4])
+            low = int(row[5])
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+        
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='blue ')
+ax.plot(dates,lows,c='red')
+plt.xlabel('Date', fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.title("Daily high and low temperatures - 2021", fontsize=24)
+
+plt.show()
