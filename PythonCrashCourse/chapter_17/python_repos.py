ed316d81b1685e0f11fe46916825f3ccd41a750b
Print-Ammar <ammaraltafsaleh2007@gmail.com>
2025-10-12 15:04:26 -0400

    Checking First Repo Using API call

--- PythonCrashCourse/chapter_17/python_repos.py
+++ PythonCrashCourse/chapter_17/python_repos.py
@@ -8,5 +8,10 @@ print(f"Status code: {r.status_code}")
 
 # Store API response in a variable
 response_dict = r.json()
-
-print(response_dict.keys())
+print(f"Total repositories: {response_dict['total_count']}")
+repo_dicts = response_dict['items']
+#Examine first repo
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
