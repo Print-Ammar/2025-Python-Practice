7edd1b5683a77cf9a44d77f1e0f3abb519135f3a
Print-Ammar <ammaraltafsaleh2007@gmail.com>
2025-10-12 15:27:49 -0400

    description for all github repos

--- PythonCrashCourse/chapter_17/python_repos.py
+++ PythonCrashCourse/chapter_17/python_repos.py
@@ -10,15 +10,14 @@ print(f"Status code: {r.status_code}")
 response_dict = r.json()
 print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
-#Examine first repo
-repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
 
-print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+print("\nSelected information about every repository:")
+for repo_dict in repo_dicts:
+    print("\nSelected information about repository:")
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
