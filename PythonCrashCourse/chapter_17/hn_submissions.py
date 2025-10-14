6c6164664e9ba44ee4371c332339a3c637f549a2
Print-Ammar <ammaraltafsaleh2007@gmail.com>
2025-10-13 20:27:43 -0400

    hn api caller completed

---
+++ PythonCrashCourse/chapter_17/hn_submissions.py
@@ -0,0 +1,36 @@
+from operator import itemgetter
+
+import requests
+
+# Make api call and store response
+
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print(f"status code {r.status_code}")
+
+# Process info
+
+submission_ids = r.json()
+submission_dicts = []
+
+for submission_id in submission_ids[:30]:
+    # Make a seperate API call for each submission
+    url = f'https://hacker-news.firebaseio.com/v0/item/{submission_id}.json'
+    r = requests.get(url)
+    print(f"id: {submission_id}\tstatus: {r.status_code}")
+    response_dict = r.json()
+
+    #Build dictionary
+    submission_dict = {
+        'title' : response_dict['title'],
+        'hn_link' : response_dict['url'],
+        'comments' : response_dict.get('descendants', 0)
+    }
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),reverse=True)
+
+for submission in submission_dicts:
+    print(f"\nTitle: {submission['title']}")
+    print(f"Discussion link: {submission['hn_link']}")
+    print(f"Comments: {submission['comments']}")
