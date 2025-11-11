5fbf1cd57c3b19e30fddadaa6278b623a3d584f0
Print-Ammar <ammaraltafsaleh2007@gmail.com>
2025-11-11 15:13:33 -0500

    checker finished

--- Additional-Code/sudoku_solver.py
+++ Additional-Code/sudoku_solver.py
@@ -41,16 +41,33 @@ def indexing_empty_spaces(board):
           for column_index in range(len(board[row_index])):
                if board[row_index][column_index] == 0:
                     empty_positions.append([column_index,row_index])
-                    return (column_index, row_index)
+                    return (row_index, column_index)
     print(empty_positions)
 
 def is_current_board_valid(board, num, pos):
      
      # check row
 
-     for row_index in range(len(board[0])):
-          if board[pos[0]][row_index] and pos [1] != row_index:
+     for column_index in range(len(board[0])):
+          if board[pos[0]][column_index] == num and pos[1] != column_index:
                return False
+    
+    # check column
+
+     for row_index in range(len(board)):
+          if board[row_index][pos[1]] == num and pos[0] != row_index:
+               return False
+          
+    # check box
+
+     box_x = pos[0] // 3
+     box_y = pos[1] // 3
+
+     for x_pos in range(box_x*3, box_x*3 + 3):
+          for y_pos in (box_y*3, box_y*3 + 3):
+               if board[x_pos][y_pos] == num and (x_pos,y_pos) != pos:
+                    return False
+        
 
 print_board(board)
 count_empty_spaces(board)
