00d421c5703c70501960d6b4a405092c2c4a5bb2
Print-Ammar <ammaraltafsaleh2007@gmail.com>
2025-11-14 16:15:50 -0500

    Regex Practice

--- Additional-Code/sudoku_solver.py
+++ Additional-Code/sudoku_solver.py
@@ -1,14 +1,15 @@
 board = [
-    [1,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
+
 
 def print_board(board):
     for row_index in range(len(board)):
@@ -36,39 +37,56 @@ def count_empty_spaces(board):
 # Attempt 1
 
 def indexing_empty_spaces(board):
-    empty_positions = []
     for row_index in range(len(board)):
           for column_index in range(len(board[row_index])):
                if board[row_index][column_index] == 0:
-                    empty_positions.append([column_index,row_index])
                     return (row_index, column_index)
-    print(empty_positions)
+    return None
 
 def is_current_board_valid(board, num, pos):
      
      # check row
 
      for column_index in range(len(board[0])):
-          if board[pos[0]][column_index] == num and pos[1] != column_index:
+         if board[pos[0]][column_index] == num and pos[1] != column_index:
                return False
     
     # check column
 
      for row_index in range(len(board)):
-          if board[row_index][pos[1]] == num and pos[0] != row_index:
-               return False
+         if board[row_index][pos[1]] == num and pos[0] != row_index:
+             return False
           
     # check box
 
-     box_x = pos[0] // 3
-     box_y = pos[1] // 3
+     box_row = pos[0] // 3
+     box_column = pos[1] // 3
 
-     for x_pos in range(box_x*3, box_x*3 + 3):
-          for y_pos in (box_y*3, box_y*3 + 3):
-               if board[x_pos][y_pos] == num and (x_pos,y_pos) != pos:
-                    return False
+     for row in range(box_row*3, box_row*3 + 3):
+         for col in range(box_column*3, box_column*3 + 3):
+             if board[row][col] == num and (row,col) != pos:
+                 return False
+    
+     return True
         
+def solve(board):
+     find = indexing_empty_spaces(board)
+     if not find:
+          return True
+     else:
+          row, col = find
+    
+     for i in range(1,10):
+          if is_current_board_valid(board, i, (row,col)):
+               board[row][col] = i
+
+               if solve(board):
+                    return True
+               
+               board[row][col] = 0
+     return False
 
 print_board(board)
 count_empty_spaces(board)
-indexing_empty_spaces(board)
+solve(board)
+print_board(board)
